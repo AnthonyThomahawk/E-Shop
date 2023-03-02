926d067b607005992daecb2ba98c7d6eaabc829f
Huray-hub <panos.skiadas12@gmail.com>
2023-03-02 14:22:21 +0200

    added pagination to getProducts

--- client/src/lib/Products.ts
+++ client/src/lib/Products.ts
@@ -1,8 +1,8 @@
 import API from "./AxiosWrapper";
 
-export const getProducts = async () => {
+export const getProducts = async (page:number, pageSize:number) => {
     try {
-        return await API.get('/products');
+        return await API.get(`/api/products?page=${page}&page_size=${pageSize}`);
     } catch (error) {
         console.error(error)
     }
--- client/src/routes/Products/+page.svelte
+++ client/src/routes/Products/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import ItemFrame from './ItemFrame.svelte'
+    import { onMount } from "svelte";
+    import ItemFrame from "./ItemFrame.svelte";
     import { getProducts } from "../../lib/Products";
+    import { each } from "svelte/internal";
 
     interface IProduct {
         CategoryID: number;
@@ -15,27 +16,42 @@
     }
 
     let products: Array<IProduct> = [];
-    onMount(async() => {
-        const res = await getProducts();
+    onMount(async () => {
+        const res = await getProducts(1, 10);
 
-        products = res.map((item:any) => ({
-            CategoryID : item.CategoryID,
-            SKU : item.SKU,
-            Label : item.Label,
-            Description : item.Description,
-            Characteristics : item.Characteristics,
-            Price : item.Price,
-            Stock : item.Stock,
-            ImageURL : item.ImageURL
+        products = res.map((item: any) => ({
+            CategoryID: item.CategoryID,
+            SKU: item.SKU,
+            Label: item.Label,
+            Description: item.Description,
+            Characteristics: item.Characteristics,
+            Price: item.Price,
+            Stock: item.Stock,
+            ImageURL: item.ImageURL,
         }));
     });
-
 </script>
+
 <h1>Products</h1>
 <div style="display:flex;">
-    <ItemFrame ItemName="First item" ItemPrice="999$" ItemID="1" imagePath="./Images/svelte.svg"/>
-    <div style="padding:10px"></div>
-    <ItemFrame ItemName="Second item" ItemPrice="50$" ItemID="2" imagePath="./Images/store-icon.svg"/>
-    <div style="padding:10px"></div>
-    <ItemFrame ItemName="Third item" ItemPrice="15$" ItemID="3" imagePath="./Images/svelte.svg"/>
+    <ItemFrame
+        ItemName="First item"
+        ItemPrice="999$"
+        ItemID="1"
+        imagePath="./Images/svelte.svg"
+    />
+    <div style="padding:10px" />
+    <ItemFrame
+        ItemName="Second item"
+        ItemPrice="50$"
+        ItemID="2"
+        imagePath="./Images/store-icon.svg"
+    />
+    <div style="padding:10px" />
+    <ItemFrame
+        ItemName="Third item"
+        ItemPrice="15$"
+        ItemID="3"
+        imagePath="./Images/svelte.svg"
+    />
 </div>
