485375b0cec9ffc2c422ab6a7efdd7d7793f6832
Huray-hub <panos.skiadas12@gmail.com>
2023-03-06 00:25:51 +0200

    added (again) .svelte-kit to gitignore

--- client/.svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/Categories/[details]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/Categories/[details]/+page.svelte";
--- client/.svelte-kit/generated/client/nodes/6.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/Products/[details]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/Products/[details]/+page.svelte";
