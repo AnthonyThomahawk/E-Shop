b592b67f716181e84b9a9c61c5210fe6b5d6b9f8
AnthonyThomahawk <antonis301@gmail.com>
2023-03-06 00:06:56 +0200

    Included .svelte-kit to project

---
+++ client/.svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/Products/[details]/+page.ts";
+export { universal };
+export { default as component } from "../../../../src/routes/Products/[details]/+page.svelte";
