a5b3860c74ec832cd382d762e196ffaff264d338
Huray-hub <panos.skiadas12@gmail.com>
2023-03-29 22:05:26 +0300

    added client jwt handlers

--- client/src/lib/AxiosWrapper.ts
+++ client/src/lib/AxiosWrapper.ts
@@ -1,20 +1,21 @@
 import axios, { type AxiosInstance } from 'axios'
+import Cookies from 'js-cookie'
 
 const axiosAPI: AxiosInstance = axios.create({
     baseURL: 'http://localhost:5000'
 })
 
 const apiRequest = async (method: string, url: string, request?: {}) => {
-    const headers = {
-        authorization: ''
-    }
+    /* const headers = { */
+    /*     authorization: '' */
+    /* } */
 
     try {
         const res = await axiosAPI({
             method,
             url,
             data: request,
-            headers
+            /* headers */
         })
 
         return await Promise.resolve(res.data)
@@ -22,6 +23,26 @@ const apiRequest = async (method: string, url: string, request?: {}) => {
         return await Promise.reject(err)
     }
 }
+
+axios.interceptors.request.use((request) => {
+    const jwt = Cookies.get('jwt')
+    if (jwt) {
+        request.headers.setAuthorization(`Bearer ${jwt}`)
+    }
+    return request
+}, error => {
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use((response) => {
+    const jwt = response.headers.authorization.split(' ')[1];
+    if (jwt) {
+        Cookies.set('jwt', jwt, { httpOnly: true });
+    }
+    return response;
+}, error => {
+    return Promise.reject(error);
+});
 
 const get = (url: string) => apiRequest('get', url)
 const post = (url: string, request: {}) => apiRequest('post', url, request)
