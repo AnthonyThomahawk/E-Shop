7867a317e74bb1a25b9858335edfddb83fcb7a40
AnthonyThomahawk <antonis301@gmail.com>
2023-04-18 15:43:58 +0300

    Changed access control origin policy to allow for future deployment

--- server/cors/middleware.go
+++ server/cors/middleware.go
@@ -5,7 +5,7 @@ import "net/http"
 func Middleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
-		w.Header().Add("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 		w.Header().
